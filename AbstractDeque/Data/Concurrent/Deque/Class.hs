8e9b43e1ae55aceb61075a9622c6f34f50ec6976
Ryan Newton <rrnewton@gmail.com>
2011-11-05 04:18:32 -0400

    Intermediate checkin.  Checking in because Class.hs accidentally tickled a haddock bug or at least a bad error message.  Look at the -- * section delimiter which got moved around and stuck inside a Class.

--- AbstractDeque/Data/Concurrent/Deque/Class.hs
+++ AbstractDeque/Data/Concurrent/Deque/Class.hs
@@ -1,10 +1,35 @@
 {-# LANGUAGE TypeFamilies, CPP, TypeSynonymInstances, MultiParamTypeClasses,
-    FlexibleInstances, EmptyDataDecls #-}
+    FlexibleInstances, EmptyDataDecls, DefaultSignatures #-}
 
+{- |
+   An abstract, parameterizable interface for queues.  
+
+   The basic design involves a non-associated type family + separate
+   type classes.  This design strives to hide the extra phantom-type
+   parameters from the Class constraints and therefore from the type
+   signatures of client code.
+ -}
 module Data.Concurrent.Deque.Class where 
 
 import Prelude hiding (Bounded)
 
+
+--------------------------------------------------------------------------------
+-- * Highly parameterized Deque type(s)
+--------------------------------------------------------------------------------
+
+-- | A Deque is selected based on several choices.
+-- 
+-- Example, a work stealing deque: 
+-- 
+--  >> (Deque NT T  D S Grow elt)
+--
+-- Note: when accepting a queue as input to a function you probably
+-- never want to overconstrain by demanding a less-featureful option.
+--
+--   For example, rather than (Deque NT D T S Grow elt)
+--   You would probably want: (Deque nt D T s Grow elt)
+data family Deque lThreaded rThreaded lDbl rDbl bnd safe elt 
 
 --------------------------------------------------------------------------------
 -- * The choices that select a queue-variant.
@@ -23,44 +48,37 @@ data Bound
 data Grow
 
 -- | Choice #4 -- duplication of elements.
-data Safe  -- | Will not duplicate elements.
-data Dup   -- | Possibly duplicating elements on pop.
+data Safe  --  Will not duplicate elements.
+data Dup   --  Possibly duplicating elements on pop.
+
+-- Possible #5:
 -- data Lossy -- I know of no algorithm which would motivate having a Lossy mode.
 
 ------------------------------------------------------------
-
 -- * Aliases for more concise Deque types:
+
 type T  = Threadsafe
 type NT = Nonthreadsafe
 type S  = SingleEnd
 type D  = DoubleEnd
 
 --------------------------------------------------------------------------------
-
--- | Design: Non-associated type family + separate type classes.
---
--- This design strives to hide the extra phantom-type parameters from
--- the type signatures of client code.    
-data family Deque lThreaded rThreaded lDbl rDbl bnd safe elt 
-
--- Example, a work stealing deque: 
--- 
---  >> (Deque NT T  D S Grow elt)
---
--- Note: when accepting a queue as input to a function you probably
--- never want to overconstrain by demanding a less-featureful option.
---
---   For example, rather than (Deque NT D T S Grow elt)
---   You would probably want: (Deque nt D T s Grow elt)
+-- * Classes containing Deque operations
+--------------------------------------------------------------------------------
 
 class DequeClass d where
    newQ  :: IO (d elt)
+
+--   default newQ :: BoundedL d => IO (d elt)
+--   newQ = newBoundedQ 256
+
    -- ----------------------------------------------------------
    -- * Natural queue operations that hold for all single, 1.5, and double ended modes.
    -- | Natural push: push left
    pushL :: d elt -> elt -> IO ()
    tryPopR  :: d elt -> IO (Maybe elt)
 
+-- --------------------------------------------------------------------------------
 -- * Auxilary type classes.  In spite of hiding the extra phantom type
 --  parameters in the DequeClass, we wish to retain the ability for
 --  clients to constrain the set of implementations they work with
@@ -83,7 +101,9 @@ class DequeClass d => PushR d where
 
 class DequeClass d => BoundedL d where 
 --   newBoundedQ :: Int -> IO (d elt) -- Presently leaving it up to the implementation what range of Q sizes are accepted.
+   newBoundedQ :: Int -> IO (d elt)
    tryPushL :: d elt -> elt -> IO Bool
+
 class PushR d => BoundedR d where 
    tryPushR :: d elt -> elt -> IO Bool
 
