6885f5478c801e208cc636dc17b037dc10c3a484
Ryan Newton <rrnewton@gmail.com>
2011-11-05 16:04:23 -0400

    With a type synonym rather than data synonym it seems better to move back to having instances with the implementation, then in the separate 'DequeInstance' module to simply provide the family instance to connect the 'Deque' family to the implementation.

--- AbstractDeque/Data/Concurrent/Deque/Class/Reference.hs
+++ AbstractDeque/Data/Concurrent/Deque/Class/Reference.hs
@@ -8,22 +8,27 @@ module Data.Concurrent.Deque.Class.Reference
   newQ, pushL, pushR, tryPopR, tryPopL, tryPushL, tryPushR)
  where
 
-import Prelude hiding (Bounded)
--- import Data.Concurrent.Deque.Class
+import Prelude hiding (length)
+import qualified Data.Concurrent.Deque.Class as C
 import Data.Sequence
 import Data.IORef
 
 -- data instance Deque lt rt l r bnd safe elt = DQ 
 
-newtype SimpleDeque elt = DQ (IORef (Seq elt))
-
--- type SimpleDeque elt = Deque T T D D Grow Safe elt
+-- | Stores a size bound (if any) as well as a mutable Seq.
+data SimpleDeque elt = DQ {-# UNPACK #-} !Int !(IORef (Seq elt))
 
 newQ = do r <- newIORef empty
-	  return (DQ r)
-pushL (DQ qr) x = atomicModifyIORef qr (\s -> (x <| s, ()))
+	  return (DQ 0 r)
 
-tryPopR (DQ qr) = atomicModifyIORef qr $ \s -> 
+newBoundedQ lim = 
+  do r <- newIORef empty
+     return (DQ lim r)
+
+pushL (DQ 0 qr) x = atomicModifyIORef qr (\s -> (x <| s, ()))
+pushL (DQ n _) _ = error$ "should not call pushL on Deque with size bound "++ show n
+
+tryPopR (DQ _ qr) = atomicModifyIORef qr $ \s -> 
    case viewr s of
      EmptyR  -> (empty, Nothing)
      s' :> x -> (s', Just x)
@@ -34,19 +39,48 @@ tryPopR (DQ qr) = atomicModifyIORef qr $ \s ->
 -- 	        Nothing -> popR q
 -- 		Just x  -> return x
 
-
 --   popL q = do x <- tryPopL q 
 -- 	      case x of 
 -- 	        Nothing -> popL q
 -- 		Just x  -> return x
 
-tryPopL (DQ qr) = atomicModifyIORef qr $ \s -> 
+tryPopL (DQ _ qr) = atomicModifyIORef qr $ \s -> 
   case viewl s of
     EmptyL  -> (empty, Nothing)
     x :< s' -> (s', Just x)
 
-pushR (DQ qr) x = atomicModifyIORef qr (\s -> (s |> x, ()))
+pushR (DQ 0 qr) x = atomicModifyIORef qr (\s -> (s |> x, ()))
+pushR (DQ n _) _ = error$ "should not call pushR on Deque with size bound "++ show n
 
-tryPushL q v = pushL q v >> return True
+tryPushL q@(DQ 0 _) v = pushL q v >> return True
+tryPushL (DQ lim qr) v = 
+  atomicModifyIORef qr $ \s -> 
+     if length s == lim
+     then (s, False)
+     else (v <| s, True)
 
-tryPushR q v = pushR q v >> return True
+tryPushR q@(DQ 0 _) v = pushR q v >> return True
+tryPushR (DQ lim qr) v = 
+  atomicModifyIORef qr $ \s -> 
+     if length s == lim
+     then (s, False)
+     else (s |> v, True)
+
+--------------------------------------------------------------------------------
+--   Instances
+--------------------------------------------------------------------------------
+
+instance C.DequeClass SimpleDeque where 
+  newQ     = newQ
+  pushL    = pushL
+  tryPopR  = tryPopR
+instance C.PopL SimpleDeque where 
+  tryPopL  = tryPopL
+instance C.PushR SimpleDeque where 
+  pushR    = pushR
+
+instance C.BoundedL SimpleDeque where 
+  tryPushL = tryPushL
+instance C.BoundedR SimpleDeque where 
+  tryPushR = tryPushR
+
--- AbstractDeque/Data/Concurrent/Deque/Class/Reference/DequeInstance.hs
+++ AbstractDeque/Data/Concurrent/Deque/Class/Reference/DequeInstance.hs
@@ -5,79 +5,11 @@ module Data.Concurrent.Deque.Class.Reference.DequeInstance () where
 import Data.Concurrent.Deque.Class
 import qualified Data.Concurrent.Deque.Class.Reference as R
 
--- data instance Deque lt rt l r bnd safe elt = R.SimpleDeque elt
+--  The reference implementation is a fully general Deque.  It can
+--   thus cover the full configuration space.
+-- type instance Deque lt rt l r bnd safe elt = R.SimpleDeque elt
+
+--  The reference implementation is a general, growable Deque.  It can
+--  cover half of the configuration space.  It does not provide a
+--  bounded queue.
 type instance Deque lt rt l r bnd safe elt = R.SimpleDeque elt
-
--- Illegal type synonym family application:
--- instance DequeClass (Deque lt rt l r bnd safe) where 
-
-instance DequeClass R.SimpleDeque where 
-  newQ     = R.newQ
-  pushL    = R.pushL
-  tryPopR  = R.tryPopR
-instance PopL R.SimpleDeque where 
-  tryPopL  = R.tryPopL
-instance PushR R.SimpleDeque where 
-  pushR    = R.pushR
-instance BoundedL R.SimpleDeque where 
-  tryPushL = R.tryPushL
-instance BoundedR R.SimpleDeque where 
-  tryPushR = R.tryPushR
-
-------------------------------------------------------------
--- Testing typechecking:
-
-foo :: Deque NT NT S S Bound Safe Int
-foo = undefined 
-
-bar = tryPopR foo
-
-foo2 :: Deque lt rt l r bnd safe Int
-foo2 = undefined 
-
-bar2 = tryPopR foo
-
-
-{-
-emptydeque :: Deque a 
-emptydeque = DQ Seq.empty
-
-takefront (DQ s) = 
-  case Seq.viewl s of
-    EmptyL  -> (emptydeque, Nothing)
-    x :< s' -> (DQ s', Just x)
-
-
--}
-
-
-{-
-
-------------------------------------------------------------
--- Examples / Tests:
-
--- Test:
-data instance Deque NT NT S S Bound Safe elt = DequeL [elt]
-foo (DequeL []) = "hello"
-
-bar :: Deque T T D D Grow Safe elt -> elt
-bar q = undefined
-
-instance DequeClass (Deque T T S S Grow Safe) where 
-  pushL = undefined
-  popR  = undefined
-
-instance DequeClass (Deque T T D D Grow Safe) where 
-  pushL = undefined
-  popR  = undefined
-instance PopL       (Deque T T D D Grow Safe) where 
-  popL  = undefined
-instance PushR      (Deque T T D D Grow Safe) where 
-  pushR = undefined
-
--- The problem here is that there's no way for an implementation to 
-
-test :: (Num elt, PopL d, BoundedPushR d, Bounded d) => d elt -> IO Bool
-test x = do popL x; pushR x 3; tryPushR x 3; tryPushL x 3
-
--}
