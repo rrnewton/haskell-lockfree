cac9abb20d8d08cc6c4a696d8b9c7883c1781321
Ryan Newton <rrnewton@gmail.com>
2011-12-23 01:25:19 -0500

    Add switch based on GHC version to disable real CAS.

--- CAS/Data/CAS.hs
+++ CAS/Data/CAS.hs
@@ -1,14 +1,25 @@
-{-# LANGUAGE MagicHash, UnboxedTuples, BangPatterns, MagicHash,
+{-# LANGUAGE CPP, MagicHash, UnboxedTuples, BangPatterns, MagicHash,
     TypeSynonymInstances, FlexibleInstances, MultiParamTypeClasses #-}
 
 -- | Atomic compare and swap for IORefs and STRefs.
 module Data.CAS 
- ( casSTRef, casIORef, ptrEq,
+ ( 
+  -- Not currently provided by Fake.hs:
+  -- casSTRef, 
+   casIORef, ptrEq,
    atomicModifyIORefCAS, atomicModifyIORefCAS_,
 
    -- * Generic interface: for interoperation with `Fake` and `Foreign` alternative libraries.
    CASRef)
 where
+
+#if __GLASGOW_HASKELL__ <= 702  /* Fix to casMutVar introduced 2011.12.09 */
+
+#warning "casMutVar is not included or is bugged in your GHC, falling back to Fake version."
+
+import Data.CAS.Internal.Fake
+
+#else
 
 import Data.CAS.Internal.Class
 import GHC.IO
@@ -97,3 +108,5 @@ atomicModifyIORefCAS_ ref fn = do
       else loop val (tries-1)
    atomicModifyIORef_ ref fn = atomicModifyIORef ref (\ x -> (fn x, ()))
 -- </duplicated code>
+
+#endif
