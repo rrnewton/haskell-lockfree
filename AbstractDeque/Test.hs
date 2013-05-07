16781074f395407302d63cfa05811ac9e81bdd1b
Ryan Newton <rrnewton@gmail.com>
2013-05-07 15:46:00 -0400

    AbstractDeque: test cleanup plus enable junit xml

--- AbstractDeque/Test.hs
+++ AbstractDeque/Test.hs
@@ -12,17 +12,19 @@ import Test.Framework.Providers.HUnit     (hUnitTestToTests)
 import Test.HUnit (assert, assertEqual, Test(TestCase, TestList))
 import qualified Data.Concurrent.Deque.Tests as T
 import qualified Data.Concurrent.Deque.Reference as R
+import System.Environment (withArgs)
 
 -- Import the instances:
-import qualified Data.Concurrent.Deque.Reference.DequeInstance 
-import System.Exit
+import Data.Concurrent.Deque.Reference.DequeInstance ()
 
+test_1 :: Test
 test_1 = TestCase $ assert $ 
   do q <- R.newQ -- Select a specific implementation.
      pushR q 3
      Just x <- tryPopR q
-     assertEqual "test_1 result" x 3
+     assertEqual "test_1 result" x (3::Integer)
 
+test_2 :: Test
 test_2 = TestCase $ assert $ 
   do 
      -- Here's an example of type-based restriction of the queue implementation:
@@ -32,16 +34,18 @@ test_2 = TestCase $ assert $
      Just x <- tryPopR q
      assertEqual "test_2 result" x 33
 
+main :: IO ()
 #if __GLASGOW_HASKELL__ >= 700
 main = do 
   putStrLn "[ Test executable: test reference deque implementation... ]"
-  defaultMain$ hUnitTestToTests$ 
-      TestList $ 
-        [ 
-          T.tests_all R.newQ 
-        , test_1
-	, test_2
-        ]
+  withArgs ["-j1","--jxml=test-results.xml"] $   
+    defaultMain$ hUnitTestToTests$ 
+        TestList $ 
+          [ 
+            T.tests_all R.newQ 
+          , test_1
+          , test_2
+          ]
 #else
 main = putStrLn "WARNING: Tests disabled for GHC < 7"
 #endif
