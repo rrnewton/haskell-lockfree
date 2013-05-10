7fe8b0bf07a1d6e84ab50f892c6f61e8575fa280
Ryan Newton <rrnewton@gmail.com>
2013-05-10 14:12:48 -0400

    Cap number of threads for atomic primop testing

--- AtomicPrimops/testing/runTest.hs
+++ AtomicPrimops/testing/runTest.hs
@@ -8,6 +8,8 @@ main = do
 --  p <- getNumProcessors
   putStrLn "[TestHarness] Calling compiled test-atomic-primops executable..."
   ExitSuccess <- system "./dist/build/test-atomic-primops/test-atomic-primops +RTS -N1"
-  ExitSuccess <- system$"./dist/build/test-atomic-primops/test-atomic-primops +RTS -N"
+  -- TEMP: Fixing this at four processors because it takes a REALLY long time at larger numbers:
+  ExitSuccess <- system$"./dist/build/test-atomic-primops/test-atomic-primops +RTS -N4"
+  -- It does 248 test cases and takes 55s at -N16...
 --  ExitSuccess <- system$"./dist/build/test-atomic-primops/test-atomic-primops +RTS -N"++show p
   return ()
