67473b8938919f9f8f1928f596650acb5660e80c
Ryan Newton <rrnewton@gmail.com>
2013-05-22 20:53:42 -0400

    Finish the Debugger and add the option for ChaseLev to use it.  Having problems though.

--- AbstractDeque/Data/Concurrent/Deque/Debugger.hs
+++ AbstractDeque/Data/Concurrent/Deque/Debugger.hs
@@ -4,7 +4,7 @@
 -- invariants at runtime for debugging purposes.
 
 module Data.Concurrent.Deque.Debugger
-       ()
+       (DebugDeque(DebugDeque))
        where
 
 import Data.IORef
@@ -20,13 +20,42 @@ import Data.Concurrent.Deque.Class
 -- This extreme form of monagamy is easier to verify, because we don't have enough
 -- information to know if two operations on different threads are racing with one
 -- another or are properly synchronized.
-data DebugDeque d elt = DebugDeque (IORef (Maybe ThreadId)) (d elt) 
+data DebugDeque d elt = DebugDeque (IORef (Maybe ThreadId), IORef (Maybe ThreadId)) (d elt) 
 
 
 instance DequeClass d => DequeClass (DebugDeque d) where 
-  pushL (DebugDeque d ref) elt =
-    undefined
+  pushL (DebugDeque (ref,_) q) elt = do
+    markThread (leftThreadSafe q) ref
+    pushL q elt
 
-  newQ = do r <- newIORef Nothing
-            fmap (DebugDeque r) newQ
+  tryPopR (DebugDeque (_,ref) q) = do
+    markThread (rightThreadSafe q) ref
+    tryPopR q 
 
+  newQ = do l <- newIORef Nothing
+            r <- newIORef Nothing
+            fmap (DebugDeque (l,r)) newQ
+
+  -- FIXME: What are the threadsafe rules for nullQ?
+  nullQ (DebugDeque _ q) = nullQ q
+      
+  leftThreadSafe  (DebugDeque _ q) = leftThreadSafe q
+  rightThreadSafe (DebugDeque _ q) = rightThreadSafe q
+
+
+instance PopL d => PopL (DebugDeque d) where 
+  tryPopL (DebugDeque (ref,_) q) = do
+    markThread (leftThreadSafe q) ref
+    tryPopL q 
+
+-- | Mark the last thread to use this endpoint.
+markThread True _ = return () -- Don't bother tracking.
+markThread False ref = do
+  last <- readIORef ref
+  tid  <- myThreadId
+  atomicModifyIORef ref $ \ x ->
+    case x of
+      Nothing -> (Just tid, ())
+      Just tid2
+        | tid == tid2 -> (Just tid,())
+        | otherwise   -> error$ "DebugDeque: invariant violated, thread safety not allowed but accessed by: "++show (tid,tid2)
--- ChaseLev/Test.hs
+++ ChaseLev/Test.hs
@@ -6,9 +6,14 @@ module Main where
 import Test.Framework                  (defaultMain)
 import Test.Framework.Providers.HUnit  (hUnitTestToTests)
 import Data.Concurrent.Deque.Tests     (tests_wsqueue)
-import Data.Concurrent.Deque.ChaseLev  (newQ)
+import Data.Concurrent.Deque.ChaseLev  (ChaseLevDeque)
+import Data.Concurrent.Deque.Class     (newQ)
+import Data.Concurrent.Deque.Debugger  (DebugDeque)
+-- import Data.Concurrent.Deque.ChaseLev  (newQ)
 
 main =
   defaultMain$ hUnitTestToTests$
   -- test_all newQ
-  tests_wsqueue newQ
+  tests_wsqueue
+  (newQ :: IO (ChaseLevDeque a))
+  -- (newQ :: IO (DebugDeque ChaseLevDeque a))
