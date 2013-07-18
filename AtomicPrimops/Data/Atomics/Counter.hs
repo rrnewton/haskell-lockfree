c0dd383d5e36fafd14f92f5c4dcec00193f7e11f
Ryan Newton <rrnewton@gmail.com>
2013-07-18 12:58:05 -0400

    Change the default Atomic.Counter export to Foreign.

--- AtomicPrimops/Data/Atomics/Counter.hs
+++ AtomicPrimops/Data/Atomics/Counter.hs
@@ -2,7 +2,9 @@
 
 module Data.Atomics.Counter
        (
-         module Data.Atomics.Counter.IORef
+--         module Data.Atomics.Counter.IORef
+         module Data.Atomics.Counter.Foreign
        ) where
 
-import Data.Atomics.Counter.IORef
+--import Data.Atomics.Counter.IORef
+import Data.Atomics.Counter.Foreign
