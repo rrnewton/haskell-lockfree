032dc61b091fe8eb49ab1e179ba888d5dbe928e5
Ryan Newton <rrnewton@gmail.com>
2014-02-02 23:54:07 -0500

    Imitate travis and put the jenkins script in the repo.

---
+++ .jenkins_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source $HOME/rn_jenkins_scripts/acquire_ghc.sh
+which cabal
+cabal --version
+
+which -a llc || echo "No LLVM"
+
+# AtomicPrimops/testing/
+PKGS="atomic-primops/ atomic-primops/testing/ atomic-primops-foreign/ AbstractDeque/ MichaelScott/ ChaseLev/ MegaDeque/"
+MODE1="--enable-library-profiling --enable-executable-profiling"
+MODE2="--disable-library-profiling --disable-executable-profiling"
+
+cabal sandbox init
+CMDROOT="cabal install --reinstall --with-ghc=ghc-$JENKINS_GHC --force-reinstalls --enable-tests"
+$CMDROOT $MODE2 $PKGS
