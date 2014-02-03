572fb8d7e572001da5f2fe3ce99d3746182d786f
Ryan Newton <rrnewton@gmail.com>
2014-02-03 00:18:18 -0500

    Update to jenkins script

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -15,5 +15,16 @@ MODE1="--enable-library-profiling --enable-executable-profiling"
 MODE2="--disable-library-profiling --disable-executable-profiling"
 
 cabal sandbox init
-CMDROOT="cabal install --reinstall --with-ghc=ghc-$JENKINS_GHC --force-reinstalls --enable-tests"
+
+# First install everything without testing:
+CMDROOT="cabal install --reinstall --with-ghc=ghc-$JENKINS_GHC --force-reinstalls"
 $CMDROOT $MODE2 $PKGS
+
+echo "Everything installed, now to test."
+root=`pwd`
+for subdir in $PKGS; do 
+  cd $root/$subdir
+  cabal sandbox init --sandbox=`root`
+  # Print the individual test outputs:
+  cabal test --show-details=always
+done
