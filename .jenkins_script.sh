1313bb2de71b1c186fadaba244013a6140646489
Ryan Newton <rrnewton@gmail.com>
2014-04-10 09:51:57 -0400

    Update the jenkins script to obey env vars for different ghc build modes

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -11,8 +11,26 @@ which -a llc || echo "No LLVM"
 
 # AtomicPrimops/testing/
 PKGS="atomic-primops/ atomic-primops/testing/ atomic-primops-foreign/ abstract-deque/ abstract-deque-tests/ lockfree-queue/ chaselev-deque/ mega-deque/"
-MODE1="--enable-library-profiling --enable-executable-profiling"
-MODE2="--disable-library-profiling --disable-executable-profiling"
+
+# Pass OPTLVL directly to cabal:
+CBLARGS=" $OPTLVL "
+
+if [ "$PROF" == "prof" ]; then 
+  CBLARGS="$CBLARGS --enable-library-profiling --enable-executable-profiling"
+else
+  CBLARGS="$CBLARGS --disable-library-profiling --disable-executable-profiling"
+fi
+
+if [ "$HPC" == "hpc" ]; then 
+  CBLARGS="$CBLARGS --enable-library-coverage"
+else
+  CBLARGS="$CBLARGS --disable-library-coverage"
+fi
+
+if [ "$THREADING" == "threads" ]; then 
+  CBLARGS="$CBLARGS --ghc-options='-threaded' "
+else
+fi
 
 cabal sandbox init
 
@@ -26,10 +44,10 @@ cd "$root"
 
 # First install everything without testing:
 CMDROOT="cabal install --reinstall --with-ghc=ghc-$JENKINS_GHC --force-reinstalls"
-$CMDROOT $MODE2 $PKGS
+$CMDROOT $CBLARGS $PKGS
 
 # Now install the DEPENDENCIES for testing
-$CMDROOT $MODE2 $PKGS --enable-tests --only-dependencies
+$CMDROOT $CBLARGS $PKGS --enable-tests --only-dependencies
 
 # List what we've got:
 cabal sandbox hc-pkg list
