0f168419ba9b66cea655873891e05f3f8680b3dc
Ryan Newton <rrnewton@gmail.com>
2014-04-12 21:45:26 -0400

    Simplify Issue28, and update the atomic-primops/testing cabal file to not insert -threaded and -O2 by default.

--- atomic-primops/testing/Issue28.hs
+++ atomic-primops/testing/Issue28.hs
@@ -7,12 +7,12 @@ import Data.Atomics.Internal (ptrEq)
 main = do
   r <- newIORef "hi"
   t0 <- readForCAS r
-  (b1,t1) <- casIORef r t0 "bye"
-  putStrLn$ "First CAS succeeded? "++show b1
-  putStrLn$ "Tickets pointer equal? " ++ show (t0 == t1)
-  (b2,t2) <- casIORef r t1 "bye2"
-  putStrLn$ "Second CAS succeeded? " ++ show b2
-  unless (b1 == True) $ error "Test failed"
-
+  (True,t1) <- casIORef r t0 "bye"
+  -- putStrLn$ "First CAS succeeded? "++show b1
+  -- putStrLn$ "Tickets pointer equal? " ++ show (t0 == t1)
+  -- (b2,t2) <- casIORef r t1 "bye2"
+  -- putStrLn$ "Second CAS succeeded? " ++ show b2
+  -- unless (b1 == True) $ error "Test failed"
+  return ()
 
 
