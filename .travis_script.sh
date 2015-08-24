23c9554e6fc4826af3f833b80eee02785ee1dc97
Ryan Newton <rrnewton@gmail.com>
2015-08-23 22:22:20 -0400

    Tweak to how tests are run

--- .travis_script.sh
+++ .travis_script.sh
@@ -2,9 +2,9 @@
 
 set -xe
 
-(cd ./atomic-primops/testing && cabal test --show-details=always)
-(cd ./atomic-primops-foreign && cabal configure && cabal test --show-details=always)
-(cd ./abstract-deque   &&       cabal configure && cabal test --show-details=always)
-(cd ./lockfree-queue// &&       cabal configure && cabal test --show-details=always)
-(cd ./chaselev-deque/  &&       cabal configure && cabal test --show-details=always)
-(cd ./mega-deque/      &&       cabal configure && cabal test --show-details=always)
+(cd ./atomic-primops/testing && cabal configure --enable-tests && cabal test --show-details=always)
+(cd ./atomic-primops-foreign && cabal configure --enable-tests && cabal test --show-details=always)
+(cd ./abstract-deque   &&       cabal configure --enable-tests && cabal test --show-details=always)
+(cd ./lockfree-queue// &&       cabal configure --enable-tests && cabal test --show-details=always)
+(cd ./chaselev-deque/  &&       cabal configure --enable-tests && cabal test --show-details=always)
+(cd ./mega-deque/      &&       cabal configure --enable-tests && cabal test --show-details=always)
