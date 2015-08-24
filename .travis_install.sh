8822c0f687ef84128236c1ac3800cb26bbd77218
Ryan Newton <rrnewton@gmail.com>
2015-08-23 22:35:24 -0400

    Attempt to activate stack-based tests.  Also tweak atomic-primops-vector version requirements.

--- .travis_install.sh
+++ .travis_install.sh
@@ -2,19 +2,27 @@
 
 set -xe
 
-cabal --version
-echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
-which -a ghc
-ghc --version
+if [ "$STACKVER" == "" ]; then
+# Legacy cabal version; soon to be deprecated:
 
-cabal update # Can put a retry here...
+    cabal --version
+    echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
+    which -a ghc
+    ghc --version
 
-# This is a hack to make Travis happy because it doesn't install happy/alex by default
-# cabal install -j happy alex
+    cabal update # Can put a retry here...
 
-# And now we install the main packages:
-PKGS="./atomic-primops ./atomic-primops-foreign ./abstract-deque/ ./abstract-deque-tests/ ./lockfree-queue/ ./chaselev-deque/ ./mega-deque"
-cabal install -j $PKGS
+    # This is a hack to make Travis happy because it doesn't install happy/alex by default
+    # cabal install -j happy alex
 
-# Now enable benchmarks and tests and add the extra dependencies:
-cabal install -j --only-dependencies --enable-tests --enable-benchmarks $PKGS
+    # And now we install the main packages:
+    PKGS="./atomic-primops ./atomic-primops-foreign ./abstract-deque/ ./abstract-deque-tests/ ./lockfree-queue/ ./chaselev-deque/ ./mega-deque"
+    cabal install -j $PKGS
+
+    # Now enable benchmarks and tests and add the extra dependencies:
+    cabal install -j --only-dependencies --enable-tests --enable-benchmarks $PKGS
+else
+    # Sweet and simple:
+    stack setup --no-terminal
+    stack test --only-snapshot --no-terminal
+fi
